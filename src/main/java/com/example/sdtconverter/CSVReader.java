8fa0ce278ec80c89c7c65db2f3079e49b01a8aca
jabongg <jbpvns@gmail.com>
2021-06-04 17:13:14 +0530

    copied input excel to output excel successfully

--- src/main/java/com/example/sdtconverter/CSVReader.java
+++ src/main/java/com/example/sdtconverter/CSVReader.java
@@ -5,21 +5,74 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.formula.functions.Rows;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Iterator;
 
 public class CSVReader {
 
     public static void readCsv() throws IOException {
+        // excel : input
         File excel = new File("/Users/jpatel10/Desktop/Converter/SDT_TO_wallet_conversion.xlsx");
         FileInputStream fileInputStream = new FileInputStream(excel);
-        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+        XSSFWorkbook inputWorkbook = new XSSFWorkbook(fileInputStream);
+        XSSFSheet inputSheet = inputWorkbook.getSheetAt(0);
+
+        // formatted excel : output
+        XSSFWorkbook outputWorkbook = new XSSFWorkbook();
+        FileOutputStream fileOutputStream = new FileOutputStream("/Users/jpatel10/Desktop/Converter/formatted" + System.currentTimeMillis()+ ".xlsx");
+        XSSFSheet outputSheet = outputWorkbook.createSheet();
+
+        int rowCount = 0;
+        // iterate in rows
+        Iterator<Row> rowsIterator = inputSheet.iterator();
+        while (rowsIterator.hasNext()) {
+            //get the row
+            Row inputRow = rowsIterator.next();
+            Row outputRow = outputSheet.createRow(rowCount++);
+            // iterate cells from the current row
+            Iterator<Cell> inputCellIterator = inputRow.cellIterator();
+            Iterator<Cell> outputCellIterator = outputRow.cellIterator();
+            while (inputCellIterator.hasNext()) {
+                int cellCount = 0;
+                Cell inputCell = inputCellIterator.next();
+                System.out.println(inputCell.toString());
+
+                //logic goes here
+                Cell outputCell = outputRow.createCell(cellCount++);
+                outputCell.setCellValue(inputCell.toString());
+                //System.out.println(outputCell + " ");
+            }
+            System.out.println();
+        }
+
+        outputWorkbook.write(fileOutputStream);
+        outputWorkbook.close();
+        inputWorkbook.close();
+        fileInputStream.close();
+    }
+
+    public  static void createFormattedCsv(Workbook workbook) throws IOException {
+
+        Workbook formattedWorkbook = new XSSFWorkbook();
+        Sheet formattedSheet = formattedWorkbook.createSheet();
+
+
+        FileOutputStream fileOutputStream = new FileOutputStream("/Users/jpatel10/Desktop/Converter/formatted.xlsx");
+        formattedWorkbook.write(fileOutputStream);
+        formattedWorkbook.close();
+        workbook.close();
+    }
+
+
+    public static void writeCsv() throws IOException {
+        File excel = new File("/Users/jpatel10/Desktop/Converter/formatted.xlsx");
+        FileOutputStream fileOutputStream = new FileOutputStream(excel);
+        XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet sheet = workbook.getSheetAt(0);
 
         // iterate in rows
@@ -38,6 +91,6 @@ public class CSVReader {
         }
 
         workbook.close();
-        fileInputStream.close();
+        //fileInputStream.close();
     }
 }
