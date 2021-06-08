ff599bbac2d7249489cc8d4c651b9ee645022231
jabongg <jbpvns@gmail.com>
2021-06-08 10:36:38 +0530

    refactored : removed unnecessary code

--- src/main/java/com/example/sdtconverter/errorcodes/ErrorCodes.java
+++ src/main/java/com/example/sdtconverter/errorcodes/ErrorCodes.java
@@ -1,4 +1,4 @@
-package com.example.sdtconverter;
+package com.example.sdtconverter.errorcodes;
 
 /**
  * for error codes: refer to the wiki :https://wiki.intuit.com/pages/viewpage.action?spaceKey=EBSPaymentsStrategy&title=Converter+Service%3A+CSV+file+fields
